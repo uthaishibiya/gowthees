e9e4d3ebdf4ed8410e3137b0d40ded33b6a46b9b
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-13 17:29:34 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i;
+		String n="hello";
+		for(i=0;;i++){
+			System.out.println("hello");
+		}
+	}
+}
