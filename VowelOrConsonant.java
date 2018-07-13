b0cd1b31bc42c3b77d5e2ad02b3c521144f79b32
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-13 10:35:53 +0530

    Create VowelOrConsonant.java

---
+++ VowelOrConsonant.java
@@ -0,0 +1,13 @@
+class VowelOrConsonant
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char ch='a';
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='I'||ch=='O'||ch=='U'){
+		System.out.println(ch+"vowel");
+	}
+	else{
+		System.out.println("consonant");
+	}
+	}
+}
