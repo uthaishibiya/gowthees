12cdc4b0395e8ca385f3aca1c56ac4bf07fce515
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-13 20:57:52 +0530

    Create Integer.java

---
+++ Integer.java
@@ -0,0 +1,14 @@
+	class Integer
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a[]={1,2,3,4,5};
+int n=2;
+int i=0;
+int sum=0;
+for(i=0;i<n;i++){
+	sum+=a[i];
+}
+}
+}
+	
