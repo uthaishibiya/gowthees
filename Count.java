cbe2c538cbf0601a1c5a3a7d4c857c270a98b21e
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-13 18:01:12 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,13 @@
+class Count
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=0;
+		int n=101;
+		while(n>0){
+			n=n/10;
+			i++;
+		}
+		System.out.println(n);
+	}
+}
