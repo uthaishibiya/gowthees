8b9cdb9ab0a15d2855d3e4ce126664d1453ba6eb
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-13 14:37:40 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,20 @@
+class Alphabet
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		char ch='a';
+		
+if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
+	
+		System.out.println(ch+"alphabet");
+	
+	}
+		
+else{
+			
+System.out.println(ch+"not an alphabet");
+		
+}
+	}
+}
+
