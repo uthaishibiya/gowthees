83fc7a7c7ae51c2a9b240250f15deaf0445287c8
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-13 17:44:14 +0530

    Create Idone.java

---
+++ Idone.java
@@ -0,0 +1,13 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=1;
+		int n=5;
+		int c=0;
+		for(i=1;i<=n;i++){
+		c=c+i;
+		}
+		System.out.println(i);
+	}
+}
