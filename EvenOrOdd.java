3f56843e51b4e0e8ddf0aae394db9d3170304672
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-13 10:23:07 +0530

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,13 @@
+class EvenOrOdd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=22;
+		if((n&1)==0){
+			System.out.println("even");
+		}
+		else{
+			System.out.println("odd");
+		}
+	}
+}
