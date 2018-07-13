1dcd8c39fb67e54f428a7338ea6a93add8bf3d39
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-13 10:16:56 +0530

    Create PositiveOrNegative.java

---
+++ PositiveOrNegative.java
@@ -0,0 +1,16 @@
+class PositiveOrNegative
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=22;
+		if(a>0){
+			System.out.println("positive");
+		}
+		else if(a<0){
+			System.out.println("negative");
+		}
+		else{
+			System.out.println("zero");
+	}
+}
+}
