87b7e500f9608a2872fbacda6fc9961451c42118
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-13 22:02:02 +0530

    Create TwoPrime.java

---
+++ TwoPrime.java
@@ -0,0 +1,13 @@
+class TwoPrime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=2;
+		int a=10;
+		for(i=2;i<=10;i++){
+			if(a%1!=0){
+				System.out.println(i);
+			}
+		}
+	}
+}
