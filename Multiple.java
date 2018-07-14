77f8431040b4cbad459b70a67a49b9f58c8ff7b6
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-14 14:45:05 +0530

    Create Multiple.java

---
+++ Multiple.java
@@ -0,0 +1,15 @@
+class Multiple
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i=0;
+		int s=0;
+		int a=50;
+		for(i=0;i<a;i++){
+			if(i%5==0){
+				s=s+i;
+			}
+		}
+		System.out.println(s);
+	}
+}
