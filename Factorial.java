d77dc282fac089dc6745bc182f72db46d4d1d6f4
uthaishibiya <41136604+uthaishibiya@users.noreply.github.com>
2018-07-14 14:35:24 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+class Factorial
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i;
+		int n=5;
+		int f=1;
+		for(i=1;i<n;i++){
+			f=f*i;
+		}
+		System.out.println("factorial of a number is"+f);
+	}
+}
